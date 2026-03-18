934e149073327112d03eb38cbeff55d6d7acb0d1
jogimagiJapan <11228shu@gmail.com>
2026-03-19 06:53:41 +0900

    Update GAS_URL to new endpoint

--- src/constants/gas.ts
+++ src/constants/gas.ts
@@ -1,1 +1,1 @@
-export const GAS_URL = process.env.NEXT_PUBLIC_GAS_URL || "https://script.google.com/macros/s/AKfycbyKKyDY6CzCGNoJNm4_-0fjC2OiOAnkhBLX3xCS4A3v0V10wm8VsJ1F56nBc8-3ygRv/exec";
+export const GAS_URL = process.env.NEXT_PUBLIC_GAS_URL || "https://script.google.com/macros/s/AKfycbwwqKocq-PCZLdZtjtsvtcfv6dc82ijrKdqiVCFKPZq8TBnKPhGuKloPO4TAUEHiA-F/exec";
